bf858573d046532daa2df2c30ffe167f41481061
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-04-26 11:12:36 +0700

    Create repository for category

---
+++ repository/category_repository.go
@@ -0,0 +1,21 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"go-restful-api/model/domain"
+)
+
+// Contract for repository category
+type CategoryRespository interface {
+	// Contract function Save for insert data
+	Save(ctx context.Context, tx sql.Tx, category domain.Category) domain.Category
+	// Contract function Update for update data
+	Update(ctx context.Context, tx sql.Tx, category domain.Category) domain.Category
+	// Contract function Delete for delete data
+	Delete(ctx context.Context, tx sql.Tx, category domain.Category)
+	// Contract function FindId for find data based on id
+	FindById(ctx context.Context, tx sql.Tx, category int) domain.Category
+	// Contract function FindAll for find all data
+	FindAll(ctx context.Context, tx sql.Tx) []domain.Category
+}
