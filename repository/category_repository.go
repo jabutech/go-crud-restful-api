dc130bcf45f2b6658c1a5077fb7f92432f41827b
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-04-26 22:47:46 +0700

    Create function Find by id

--- repository/category_repository.go
+++ repository/category_repository.go
@@ -13,9 +13,9 @@ type CategoryRespository interface {
 	// Contract function Update for update data
 	Update(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category
 	// Contract function Delete for delete data
-	Delete(ctx context.Context, tx *sql.Tx, category domain.Category)
+	Delete(ctx context.Context, tx *sql.Tx, category domain.Category) string
 	// Contract function FindId for find data based on id
-	FindById(ctx context.Context, tx *sql.Tx, categoryId int) domain.Category
+	FindById(ctx context.Context, tx *sql.Tx, categoryId int) (domain.Category, error)
 	// Contract function FindAll for find all data
 	FindAll(ctx context.Context, tx *sql.Tx) []domain.Category
 }
--- repository/category_repository_impl.go
+++ repository/category_repository_impl.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"go-restful-api/helper"
 	"go-restful-api/model/domain"
 )
@@ -42,9 +43,54 @@ func (repository *CategoryRepositoryImpl) Update(ctx context.Context, tx *sql.Tx
 	// (2) Create context
 	_, err := tx.ExecContext(ctx, SQL, category.Name, category.Id)
 
-	// (3) If error, handler with helper error
+	// (3) If error, handle with helper error
 	helper.PanicErr(err)
 
 	// (4) If success, return category
 	return category
+}
+
+// Function Delete with follow the contract category repository
+func (repository CategoryRepositoryImpl) Delete(ctx context.Context, tx *sql.Tx, category domain.Category) string {
+	// (1) Create sql query
+	SQL := "delete from category where id = ?"
+
+	// (2) Create context
+	_, err := tx.ExecContext(ctx, SQL, category.Id)
+
+	// (3) If error handle with helper error
+	helper.PanicErr(err)
+
+	// (4) Return info success deleted
+	return "Delete success."
+}
+
+// Function Find data by id with follow the contract category repository
+func (repository CategoryRepositoryImpl) FindById(ctx context.Context, tx *sql.Tx, categoryId int) (domain.Category, error) {
+	// (1) Create sql query
+	SQL := "select id, name from category where id = ?"
+
+	// (2) Create query context
+	rows, err := tx.QueryContext(ctx, SQL, categoryId)
+
+	// (3) If error, handle with helper error
+	helper.PanicErr(err)
+
+	// (4) Create variable category with value struct domain category
+	category := domain.Category{}
+
+	// (5) If category is available
+	if rows.Next() {
+		// (1) Get data category
+		err := rows.Scan(&category.Id, &category.Name)
+
+		// (2) If error, handle with helper error
+		helper.PanicErr(err)
+
+		// (3) If no, return category with error nil
+		return category, nil
+	} else {
+		// If category is empty, return category and send info error
+		return category, errors.New("category is not found")
+	}
 }
