e40f217d3d292a8ea451ba61f4863b491365ad6f
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-04-28 03:23:33 +0700

    Added helper mode.go for handle return response category crud

---
+++ helper/model.go
@@ -0,0 +1,13 @@
+package helper
+
+import (
+	"go-restful-api/model/domain"
+	"go-restful-api/model/web"
+)
+
+func ToCategoryResponse(category domain.Category) web.CategoryResponse {
+	return web.CategoryResponse{
+		Id:   category.Id,
+		Name: category.Name,
+	}
+}
