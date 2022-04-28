9758ac6e9104e5095882aaa9d7a4c78d8acc5ff2
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-04-28 10:49:55 +0700

    feature: added function controller update category

--- controller/category_controller_impl.go
+++ controller/category_controller_impl.go
@@ -6,6 +6,7 @@ import (
 	"go-restful-api/model/web"
 	"go-restful-api/service"
 	"net/http"
+	"strconv"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -26,6 +27,47 @@ func (controller *CategoryControllerImpl) Create(writer http.ResponseWriter, req
 
 	// (5) Create new category use service Create
 	categoryResponse := controller.CategoryService.Create(request.Context(), categoryCreateRequest)
+
+	// (6) If success, create response with helper web response
+	webWebResponse := web.WebResponse{
+		Code:   http.StatusOK,
+		Status: "Ok",
+		Data:   categoryResponse,
+	}
+
+	// (7) Add header
+	writer.Header().Add("Content-Type", "application/json")
+	// (8) Create encode
+	encoder := json.NewEncoder(writer)
+	// (9) Encode web response
+	err = encoder.Encode(webWebResponse)
+	// (10) If error, handle with helper
+	helper.PanicErr(err)
+
+}
+
+func (controller *CategoryControllerImpl) Update(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	// (1) Decode request
+	decoder := json.NewDecoder(request.Body)
+	// (2) Create variable with value web.CategoryUpdateRequest{
+	categoryUpdateRequest := web.CategoryUpdateRequest{}
+	// (3) Decode request to category request struct
+	err := decoder.Decode(&categoryUpdateRequest)
+	// (4) If error, handle with helper
+	helper.PanicErr(err)
+
+	// (5) Get parameter id
+	categoryId := params.ByName("categoryId")
+	// (6) Convert to string
+	id, err := strconv.Atoi(categoryId)
+	// (7) If error, handle with helper
+	helper.PanicErr(err)
+
+	// (8) Parse parameter id to categoryUpdateRequest
+	categoryUpdateRequest.Id = id
+
+	// (5) Update category use service Update
+	categoryResponse := controller.CategoryService.Update(request.Context(), categoryUpdateRequest)
 
 	// (6) If success, create response with helper web response
 	webWebResponse := web.WebResponse{
