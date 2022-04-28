58115b6757e2ec484275e4eab850b5ba82e9275e
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-04-28 10:12:57 +0700

    Feature: Added validator when create and update category

--- model/web/category_create_request.go
+++ model/web/category_create_request.go
@@ -2,5 +2,5 @@ package web
 
 // Struct for request create new data
 type CategoryCreateRequest struct {
-	Name string
+	Name string `validate:"required,max=200,min=1"`
 }
--- service/category_service_impl.go
+++ service/category_service_impl.go
@@ -7,57 +7,70 @@ import (
 	"go-restful-api/model/domain"
 	"go-restful-api/model/web"
 	"go-restful-api/repository"
+
+	"github.com/go-playground/validator"
 )
 
 type CategoryServiceImpl struct {
-	CategoryRepository repository.CategoryRepository
-	DB                 *sql.DB
+	CategoryRepository repository.CategoryRepository // Use repository
+	DB                 *sql.DB                       // Use Sql driver
+	Validate           *validator.Validate           // Use validator
 }
 
 // Function service for proses create new category
 func (service *CategoryServiceImpl) Create(ctx context.Context, request web.CategoryCreateRequest) web.CategoryResponse {
-	// (1) Create transactional database
-	tx, err := service.DB.Begin()
-	// (2) Handle if create transaction error
+	// (1) Run validate before create data
+	err := service.Validate.Struct(request)
+	// (2) If error, handle with helper
 	helper.PanicErr(err)
-	// (3) Run this process in the end all operation with defer, and check process transaction Commit or Rollback transaction
+
+	// (3) Create transactional database
+	tx, err := service.DB.Begin()
+	// (4) Handle if create transaction error
+	helper.PanicErr(err)
+	// (5) Run this process in the end all operation with defer, and check process transaction Commit or Rollback transaction
 	defer helper.CommitOrRollback(tx)
 
-	// (4) Create new object category
+	// (6) Create new object category
 	category := domain.Category{
 		// Set name from request
 		Name: request.Name,
 	}
 
-	// (5) Save transaction with use Repository
+	// (7) Save transaction with use Repository
 	category = service.CategoryRepository.Save(ctx, tx, category)
 
-	// (6) Return after success
+	// (8) Return after success
 	return helper.ToCategoryResponse(category)
 }
 
 // Function service for proses update category
 func (service *CategoryServiceImpl) Update(ctx context.Context, request web.CategoryUpdateRequest) web.CategoryResponse {
-	// (1) Create transactional database
-	tx, err := service.DB.Begin()
-	// (2) Handle if create transaction error
+	// (1) Run validate before create data
+	err := service.Validate.Struct(request)
+	// (2) If error, handle with helper
 	helper.PanicErr(err)
-	// (3) Run this process in the end all operation with defer, and check process transaction Commit or Rollback transaction
+
+	// (3) Create transactional database
+	tx, err := service.DB.Begin()
+	// (4) Handle if create transaction error
+	helper.PanicErr(err)
+	// (5) Run this process in the end all operation with defer, and check process transaction Commit or Rollback transaction
 	defer helper.CommitOrRollback(tx)
 
-	// (2) Find category in dataabase
+	// (6) Find category in dataabase
 	category, err := service.CategoryRepository.FindById(ctx, tx, request.Id)
 
-	// (3) If error / category not found handle error with helper
+	// (7) If error / category not found handle error with helper
 	helper.PanicErr(err)
 
-	// (4) If no error, set request name to object category
+	// (8) If no error, set request name to object category
 	category.Name = request.Name
 
-	// (5) Update category with use Repository
+	// (9) Update category with use Repository
 	category = service.CategoryRepository.Update(ctx, tx, category)
 
-	// (4) Return response with helper
+	// (10) Return response with helper
 	return helper.ToCategoryResponse(category)
 }
 
