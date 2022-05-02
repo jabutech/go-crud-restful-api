d80c5c731be9669bac471c922439b2cc1818bc3a
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-05-02 21:06:20 +0700

    Added json attribute on all model web response

--- model/web/category_create_request.go
+++ model/web/category_create_request.go
@@ -2,5 +2,5 @@ package web
 
 // Struct for request create new data
 type CategoryCreateRequest struct {
-	Name string `validate:"required,max=200,min=1"`
+	Name string `validate:"required,max=200,min=1" json:"name"`
 }
