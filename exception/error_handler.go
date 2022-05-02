faabaa220ec889285570fc9502edaa4ec0d45ad0
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-05-03 06:02:47 +0700

    feature: added error handler for VALIDATION ERRORS

--- exception/error_handler.go
+++ exception/error_handler.go
@@ -4,6 +4,8 @@ import (
 	"go-restful-api/helper"
 	"go-restful-api/model/web"
 	"net/http"
+
+	"github.com/go-playground/validator"
 )
 
 func ErrorHandler(writer http.ResponseWriter, request *http.Request, err interface{}) {
@@ -12,7 +14,31 @@ func ErrorHandler(writer http.ResponseWriter, request *http.Request, err interfa
 		return
 	}
 
+	if validationErrors(writer, request, err) {
+		return
+	}
+
 	internalServerError(writer, request, err)
+}
+
+func validationErrors(writer http.ResponseWriter, request *http.Request, err interface{}) bool {
+	exception, ok := err.(validator.ValidationErrors)
+	if ok {
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusBadRequest)
+
+		webResponse := web.WebResponse{
+			Code:   http.StatusBadRequest,
+			Status: "BAD REQUEST",
+			Data:   exception.Error(),
+		}
+
+		helper.WriteToResponseBody(writer, webResponse)
+
+		return true
+	} else {
+		return false
+	}
 }
 
 func notFoundError(writer http.ResponseWriter, request *http.Request, err interface{}) bool {
