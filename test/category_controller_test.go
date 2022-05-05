40f51a4b40b863c98e1f24bd3ccf2e3e2b89b6e5
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-05-05 14:10:46 +0700

    test: added function for truncate table category before run testt

--- test/category_controller_test.go
+++ test/category_controller_test.go
@@ -36,59 +36,66 @@ func setupTestDB() *sql.DB {
 	return db
 }
 
-// Function for handle router endpoint
-func setupRouter() http.Handler {
-	// (1) Run open connection db with function setupTestDB
-	db := setupTestDB()
-	// (2) Use validator
+// Function for handle router endpoint with parameter connetion to db
+func setupRouter(db *sql.DB) http.Handler {
+	// (1) Use validator
 	validate := validator.New()
 
-	// (3) Endpoint
+	// (2) Endpoint
 	categoryRespository := repository.NewCategoriRepository()
 	categoryService := service.NewCategoryService(categoryRespository, db, validate)
 	categoryController := controller.NewCategoryController(categoryService)
 
-	// (4) Use file router
+	// (3) Use file router
 	router := app.NewRouter(categoryController)
 
-	// (5) Return router with handle middleware
+	// (4) Return router with handle middleware
 	return middleware.NewAuthMiddleware(router)
+}
+
+// Function for truncate table category
+func truncateCategory(db *sql.DB) {
+	db.Exec("TRUNCATE category")
 }
 
 // Function test for create category success
 func TestCreateCategorySuccess(t *testing.T) {
-	// (1) Use router
-	router := setupRouter()
+	// (1) Use connetion to db
+	db := setupTestDB()
+	// (2) Run truncate table category before test
+	truncateCategory(db)
+	// (3) Use router
+	router := setupRouter(db)
 
-	// (2) Create request body payload
+	// (4) Create request body payload
 	requestBody := strings.NewReader(`{"name": "Gadget"}`)
-	// (3) Create test request
+	// (5) Create test request
 	request := httptest.NewRequest(http.MethodPost, "http://localhost:3000/api/categories", requestBody)
-	// (3) Added header content type
+	// (6) Added header content type
 	request.Header.Add("Content-Type", "application/json")
-	// (4) Added header authorize
+	// (7) Added header authorize
 	request.Header.Add("X-API-Key", "RAHASIA")
 
-	// (5) Create new recorder for writer
+	// (8) Create new recorder for writer
 	recorder := httptest.NewRecorder()
 
-	// (6) Run test with send request
+	// (9) Run test with send request
 	router.ServeHTTP(recorder, request)
 
-	// (7) Get result test and save to variable response
+	// (10) Get result test and save to variable response
 	response := recorder.Result()
 
-	// (8) Read response body json
+	// (11) Read response body json
 	body, _ := io.ReadAll(response.Body)
-	// (9) Create variable responseBody with value map for response body
+	// (12) Create variable responseBody with value map for response body
 	var responseBody map[string]interface{}
-	// (10) Decode json
+	// (13) Decode json
 	json.Unmarshal(body, &responseBody)
 
-	// (11) Check response code must be 200
+	// (14) Check response code must be 200
 	assert.Equal(t, 200, int(responseBody["code"].(float64)))
-	// (12) Check response status must be `OK`
+	// (15) Check response status must be `OK`
 	assert.Equal(t, "OK", responseBody["status"])
-	// (13) Check response data must be gadget, and convert to type map
+	// (16) Check response data must be gadget, and convert to type map
 	assert.Equal(t, "Gadget", responseBody["data"].(map[string]interface{})["name"])
 }
