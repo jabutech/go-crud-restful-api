d33720054a86ed9f465910af30473cfd4973afb3
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-05-08 04:07:38 +0700

    test: Added test Update category failed and get category success

--- test/category_controller_test.go
+++ test/category_controller_test.go
@@ -205,3 +205,113 @@ func TestUpdateCategorySuccess(t *testing.T) {
 	// (17) Check response data name must be gadget, and convert to type map
 	assert.Equal(t, "T SHIRT", responseBody["data"].(map[string]interface{})["name"])
 }
+
+// Function test for update category failed
+func TestUpdateCategoryFailed(t *testing.T) {
+	// (1) Use connetion to db
+	db := setupTestDB()
+	// (2) Run truncate table category before test
+	truncateCategory(db)
+
+	// (3) Create new data for sample update
+	// (3.1) Create database transactional
+	tx, _ := db.Begin()
+	// (3.2) Use repository
+	categoryRepository := repository.NewCategoriRepository()
+	// (3.3) Create new category
+	category := categoryRepository.Save(context.Background(), tx, domain.Category{
+		Name: "Gadget",
+	})
+	// (3.4) Commit transaction
+	tx.Commit()
+
+	// (4) Use router
+	router := setupRouter(db)
+
+	// (4) Create request body payload update
+	requestBody := strings.NewReader(`{"name": ""}`)
+	// (5) Create test request update with id
+	request := httptest.NewRequest(http.MethodPut, "http://localhost:3000/api/categories/"+strconv.Itoa(category.Id), requestBody)
+	// (6) Added header content type
+	request.Header.Add("Content-Type", "application/json")
+	// (7) Added header authorize
+	request.Header.Add("X-API-Key", "RAHASIA")
+
+	// (8) Create new recorder for writer
+	recorder := httptest.NewRecorder()
+
+	// (9) Run test with send request
+	router.ServeHTTP(recorder, request)
+
+	// (10) Get result test and save to variable response
+	response := recorder.Result()
+
+	// (11) Read response body json
+	body, _ := io.ReadAll(response.Body)
+	// (12) Create variable responseBody with value map for response body
+	var responseBody map[string]interface{}
+	// (13) Decode json
+	json.Unmarshal(body, &responseBody)
+
+	// (14) Response status code must be 200 (success)
+	assert.Equal(t, 400, response.StatusCode)
+	// (15) Check response body code must be 200 (success)
+	assert.Equal(t, 400, int(responseBody["code"].(float64)))
+	// (16) Check response status must be `OK`
+	assert.Equal(t, "BAD REQUEST", responseBody["status"])
+}
+
+// Function test for get category success
+func TestGETCategorySuccess(t *testing.T) {
+	// (1) Use connetion to db
+	db := setupTestDB()
+	// (2) Run truncate table category before test
+	truncateCategory(db)
+
+	// (3) Create new data for sample update
+	// (3.1) Create database transactional
+	tx, _ := db.Begin()
+	// (3.2) Use repository
+	categoryRepository := repository.NewCategoriRepository()
+	// (3.3) Create new category
+	category := categoryRepository.Save(context.Background(), tx, domain.Category{
+		Name: "Gadget",
+	})
+	// (3.4) Commit transaction
+	tx.Commit()
+
+	// (4) Use router
+	router := setupRouter(db)
+
+	// (5) Create test request update with id
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:3000/api/categories/"+strconv.Itoa(category.Id), nil)
+	// (7) Added header authorize
+	request.Header.Add("X-API-Key", "RAHASIA")
+
+	// (8) Create new recorder for writer
+	recorder := httptest.NewRecorder()
+
+	// (9) Run test with send request
+	router.ServeHTTP(recorder, request)
+
+	// (10) Get result test and save to variable response
+	response := recorder.Result()
+
+	// (11) Read response body json
+	body, _ := io.ReadAll(response.Body)
+	// (12) Create variable responseBody with value map for response body
+	var responseBody map[string]interface{}
+	// (13) Decode json
+	json.Unmarshal(body, &responseBody)
+
+	// (14) Response status code must be 200 (success)
+	assert.Equal(t, 200, response.StatusCode)
+	// (15) Check response body code must be 200 (success)
+	assert.Equal(t, 200, int(responseBody["code"].(float64)))
+	// (16) Check response status must be `OK`
+	assert.Equal(t, "OK", responseBody["status"])
+	// (17) Check response data id must be same as the id above, and convert to type map
+	assert.Equal(t, category.Id, int(responseBody["data"].(map[string]interface{})["id"].(float64)))
+	// (17) Check response data name must be gadget, and convert to type map
+	assert.Equal(t, "Gadget", responseBody["data"].(map[string]interface{})["name"])
+}
